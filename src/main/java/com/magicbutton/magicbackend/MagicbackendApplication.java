d888301d4c39257ea33830185e228139dfd991de
samuel <samusplay08@gmail.com>
2025-09-26 09:20:16 -0500

    first commit

---
+++ src/main/java/com/magicbutton/magicbackend/MagicbackendApplication.java
@@ -0,0 +1,13 @@
+package com.magicbutton.magicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicbutton/magicbackend/MagicbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicbutton.magicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
